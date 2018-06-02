abd8714d80f3f8815c1a4cd58138e4ea9b053ee2
Joshua Masland-Sarani <j.masland@gmail.com>
2018-06-01 20:49:31 -0400

    add normalization of scores

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,4 +4,8 @@ class Match < ApplicationRecord
   belongs_to :game
   has_many :scores, dependent: :destroy
   has_many :teams, through: :scores
+
+  def max_score
+    scores.map { |s| s.score }.max
+  end
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -3,4 +3,8 @@ class Score < ApplicationRecord
   belongs_to :match
 
   has_one :game, through: :match
+
+  def normalized(max_score)
+    100 * score / max_score
+  end
 end

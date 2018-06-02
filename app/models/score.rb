6d04dbbf6041e8d1249cd1e9f04f8318e3f891f7
Joshua Masland-Sarani <j.masland@gmail.com>
2018-06-01 21:13:35 -0400

    add team score totals

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,7 +4,11 @@ class Score < ApplicationRecord
 
   has_one :game, through: :match
 
-  def normalized(max_score)
+  def normalized(max_score=nil)
+    if (!max_score)
+      max_score = match.max_score
+    end
+
     100 * score / max_score
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,8 @@ class Team < ApplicationRecord
 
   has_many :scores, dependent: :destroy
   has_many :matches, through: :scores
+
+  def total_score
+    scores.sum { |s| s.normalized }
+  end
 end
